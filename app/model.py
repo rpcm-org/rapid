8daee0cb1edd563e726fc7a4399c4870ad197870
celestian <petr.celestian@gmail.com>
2018-12-02 19:52:54 +0100

    user: uuid --> int

--- app/model.py
+++ app/model.py
@@ -10,6 +10,6 @@ class User(DB.Model):
 
     __tablename__ = 'users'
 
-    id = DB.Column(UUID, primary_key=True, default=uuid.uuid4)  # pylint: disable=invalid-name
+    id = DB.Column(DB.Integer, primary_key=True)  # pylint: disable=invalid-name
     steam_id = DB.Column(DB.String(50), nullable=False, unique=True)
     created_at = DB.Column(DB.DateTime())
